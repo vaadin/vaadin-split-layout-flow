03d601366d928dd47b6163e197dcc46b44f329f2
Tulio Garcia <tulio@vaadin.com>
2020-03-31 18:36:48 +0300

    Remove unused import

--- vaadin-split-layout-flow/src/main/java/com/vaadin/flow/component/splitlayout/SplitLayout.java
+++ vaadin-split-layout-flow/src/main/java/com/vaadin/flow/component/splitlayout/SplitLayout.java
@@ -23,7 +23,6 @@ import com.vaadin.flow.component.HasSize;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.dom.ElementConstants;
 import com.vaadin.flow.shared.Registration;
 
 /**
